b6749e208317126db4d35bd28fea957e48dedbe3
Rodrigo Turatti <rodrigo@turatti.dev>
2023-02-28 02:31:39 -0300

    Ajustes user-data.sh

--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -9,4 +9,6 @@ systemctl enable --now docker >> $LOG
 systemctl start docker >> $LOG
 mkdir /app && cd /app
 wget https://raw.githubusercontent.com/rturatti/Desafio_Globo/develop/app/docker-compose.yaml
-docker-compose up -d && sleep 20s && docker run --rm -it --network app_globo rodrigoturatti/curl-globo >> $LOG
+docker-compose up -d >> $LOG
+sleep 20s >> $LOG
+docker run --rm -it --network app_globo rodrigoturatti/curl-globo >> $LOG
